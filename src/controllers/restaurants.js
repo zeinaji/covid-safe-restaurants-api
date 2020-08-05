9a6e3417c56f5a19b8b14da8b8bd358429838ac2
Justin Sandahl <sandahl.j.d@gmail.com>
2020-08-05 18:37:19 +0100

    added GET route for the restaurants database, this is untested as unable to get database running to test on my machine.

--- src/controllers/restaurants.js
+++ src/controllers/restaurants.js
@@ -35,3 +35,34 @@ exports.create = (req, res) => {
     res.status(201).json(restaurant);
   });
 };
+
+exports.list = (req, res) => {
+  const query = Restaurant.find();
+
+  if (req.query.name) {
+    query.where('name', new RegExp(req.query.name, 'i'));
+  } else if (req.query.type) {
+    query.where('type').equals(req.query.type);
+  } else if (req.query.onDeliveroo) {
+    query.where('onDeliveroo').equals(req.query.onDeliveroo);
+  } else if (req.query.onJustEat) {
+    query.where('onJustEat').equals(req.query.onJustEat);
+  } else if (req.query.onUberEats) {
+    query.where('onUberEats').equals(req.query.onUberEats);
+  } else if (req.query.isOpen) {
+    query.where('isOpen').equals(req.query.isOpen);
+  } else if (req.query.eatOutToHelpOut) {
+    query.where('eatOutToHelpOut').equals(req.query.eatOutToHelpOut);
+  } else if (req.query.outsideSeating) {
+    query.where('outsideSeating').equals(req.query.outsideSeating);
+  }
+
+  query
+    .exec()
+    .then(restaurants => {
+      res.status(200).json(restaurants);
+    })
+    .catch(error => {
+      res.sendStatus(500);
+    });
+};
