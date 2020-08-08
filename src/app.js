d8bd27cd4edefae59ecb482ccec6f529b7aca723
Justin Sandahl <sandahl.j.d@gmail.com>
2020-08-08 13:38:04 +0100

    added method-override as a dependency this is used by the express app to allow overriding using query values

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const methodOverride = require('method-override');
 const restify = require('express-restify-mongoose');
 const restaurantModel = require('./models/restaurants');
 const favouritesModel = require('./models/favourites');
@@ -9,6 +10,7 @@ const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
+app.use(methodOverride());
 app.use(router);
 
 //endpoint === /api/v1/restaurant for GET, POST, PUT and DELETE
