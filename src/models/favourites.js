e0dea1b6c91cd0bc3e98f6d5e474974c1a8dc4e0
Zein Aji <zeinaji97@gmail.com>
2020-08-27 18:59:12 +0100

    added a rating field to the database

--- src/models/favourites.js
+++ src/models/favourites.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const favouriteSchema = new mongoose.Schema({
   fbUserId: { type: String, require: true },
   restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', require: true },
+  rating: Number,
 });
 
 const favouritesModel = mongoose.model('Favourite', favouriteSchema);
